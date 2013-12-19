b3a4a2bceb699998507f8ab9e9fd73312ae6cf71
srkaas <stevenkaas@gmail.com>
2013-12-19 14:12:41 -0800

    Changed text, moved display method.

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
+# 2D Array.
 class Matrix
   attr_accessor :columns, :width, :height
+
   def initialize(width, height, &default_contents)
     @columns = Array.new(width) { |x| Array.new(height) { |y| default_contents.call(x, y) } }
     @width = width
@@ -19,10 +21,27 @@ class Matrix
       yield column
     end
   end
+  def display
+    each_row do |row|
+      row.each do |entry|
+        if entry.is_a?(Float)
+          representation = sprintf('%.2f', entry)
+        else
+          representation = entry.to_s
+        end
+        print representation
+        number_of_padding_spaces = 6 - representation.size
+        number_of_padding_spaces.times do
+          print ' '
+        end
+      end
+      print "\n"
+    end
+  end
 end
 
 # This will be the main function
-def fill_2d_array_with_noise(parameters)
+def matrix_with_noise(parameters)
   # x counts from left to right, starting at 0
   # y counts from up to down, starting at 0
   world = Matrix.new(parameters[:width], parameters[:height]) { |x, y| rand }
@@ -33,27 +52,8 @@ def fill_2d_array_with_noise(parameters)
   return world 
 end
 
-# Print an array of arrays (all assumed to be the same size) as a list of columns
-def print_2d_array(world)
-  world.each_row do |row|
-    row.each do |entry|
-      if entry.is_a?(Float)
-        representation = sprintf('%.2f', entry)
-      else
-        representation = entry.to_s
-      end
-      print representation
-      number_of_padding_spaces = 6 - representation.size
-      number_of_padding_spaces.times do
-        print ' '
-      end
-    end
-    print "\n"
-  end
-end
-
 def test_me_with(parameters)
-  print_2d_array(fill_2d_array_with_noise(parameters))
+  matrix_with_noise(parameters).display
 end
 
 test_me_with({width: 5, height: 10, method: 'blank'})
