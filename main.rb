71b1c0a03f4b0acdefad33a780bbdab65fcd9f43
srkaas <stevenkaas@gmail.com>
2013-12-19 14:00:39 -0800

    Matrix class. Now displaying floats.

--- main.rb
+++ main.rb
@@ -1,8 +1,31 @@
+class Matrix
+  attr_accessor :columns, :width, :height
+  def initialize(width, height, &default_contents)
+    @columns = Array.new(width) { |x| Array.new(height) { |y| default_contents.call(x, y) } }
+    @width = width
+    @height = height
+  end
+  def each_row
+    @columns[0].each_with_index do |entry, row_number|
+      current_row = []
+      @columns.each do |column|
+        current_row << column[row_number]
+      end
+      yield current_row
+    end
+  end
+  def each_column
+    @columns.each do |column|
+      yield column
+    end
+  end
+end
+
 # This will be the main function
 def fill_2d_array_with_noise(parameters)
-  #x counts from left to right, starting at 0
-  #y counts from up to down, starting at 0
-  world = Array.new(parameters[:x_size]) {|x| Array.new(parameters[:y_size]) {|y| x + 10*y} }
+  # x counts from left to right, starting at 0
+  # y counts from up to down, starting at 0
+  world = Matrix.new(parameters[:width], parameters[:height]) { |x, y| rand }
   if parameters[:method] == 'perlin'
   elsif parameters[:method] == 'worley'
   elsif parameters[:method] == 'diamond-square'
@@ -12,10 +35,15 @@ end
 
 # Print an array of arrays (all assumed to be the same size) as a list of columns
 def print_2d_array(world)
-  world[0].each_with_index do |entry, row_number|
-    world.each do |column|
-      print "#{column[row_number]}"
-      number_of_padding_spaces = 4 - column[row_number].to_s.size
+  world.each_row do |row|
+    row.each do |entry|
+      if entry.is_a?(Float)
+        representation = sprintf('%.2f', entry)
+      else
+        representation = entry.to_s
+      end
+      print representation
+      number_of_padding_spaces = 6 - representation.size
       number_of_padding_spaces.times do
         print ' '
       end
@@ -28,4 +56,4 @@ def test_me_with(parameters)
   print_2d_array(fill_2d_array_with_noise(parameters))
 end
 
-test_me_with({x_size: 10, y_size: 10, method: 'blank'})
+test_me_with({width: 5, height: 10, method: 'blank'})
